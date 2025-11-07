3a6335b8fea78e05f5e0e62e90a966d214fadf0c
Sakthi <sakthikrishkmv@gmail.com>
2025-11-07 18:46:12 +0530

    Ifelse

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
-import java.awt.*;
-
-class Totto {
-
-    Totto(){
-        Frame f1 = new Frame();
-
-        Button b1 = new Button("Submit");
-
-        f1.add(b1);
-        f1.setLayout(new FlowLayout());
-        f1.setSize(200,300);
-        f1.setVisible(true);
-
-    }
-}
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Totto t1 = new Totto();
-
+    Scanner sc = new Scanner (System.in);
+    int StuMark = sc.nextInt();
+    int AttPer = sc.nextInt();
+    int FamInc = sc.nextInt();
+    if(StuMark >= 85 && AttPer >=75 && FamInc<=200000){
+        System.out.println("The Student is Eligible For The Scholarship");
+    }
+    else if (StuMark >= 90 && AttPer >=85){
+        System.out.println("The Student Is Eligible For The Full Scholarship");
+    }
+    else if (StuMark >= 80 && AttPer >=70){
+        System.out.println("The Student is Eligible For the Partial Scholarship");
+    }
+    else {
+        System.out.println("Not Eligible for the Scholarship");
     }
     }
+}
