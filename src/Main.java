dac20d9fc66fe657e042c82f138d991efe9c02d5
Ckti144 <sakthikrishkmv@gmail.com>
2025-11-08 17:10:34 +0530

    Muladded

--- src/Main.java
+++ src/Main.java
@@ -24,11 +24,25 @@ class  sub{
     }
 }
 
+class  mul{
+    public void showmul() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number:");
+        int a = sc.nextInt();
+        System.out.println("Enter the Second Number");
+        int b = sc.nextInt();
+        int c = a - b;
+        System.out.println("The Subraction of the Two Numbers is"+c);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
                 add a1 = new add();
                 a1.showadd();
                 sub s1 = new sub();
                 s1.showsub();
+                mul m1 = new mul();
+                m1.showmul();
     }
 }
