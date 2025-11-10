1d33bcef8989191560127eaa30c3ec43573fb8b8
Sakthi <sakthikrishkmv@gmail.com>
2025-11-10 21:41:49 +0530

    LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 
+import java.util.LinkedList;
 import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
-    Stack<Integer> Src = new Stack<>();
+    LinkedList<Integer> Src = new LinkedList<>();
     Src.push(3);
     Src.push(4);
     Src.add(5);
-    System.out.println(Src.peek());
-    Src.pop();
+    System.out.println(Src);
+    Src.remove(2);
     System.out.println(Src);
     }
 }
