5d338e66c4ca379bce657cb90638a9cef499f5b4
Sakthi <sakthikrishkmv@gmail.com>
2025-11-10 21:49:47 +0530

    Hashset

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
+import java.util.HashSet;
 
-import java.util.LinkedList;
-import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
-    LinkedList<Integer> Src = new LinkedList<>();
-    Src.push(3);
-    Src.push(4);
-    Src.add(5);
-    System.out.println(Src);
-    Src.remove(2);
-    System.out.println(Src);
+        HashSet<Integer> hash = new HashSet<>();
+        hash.add(2);
+        hash.add(3);
+        hash.add(2);
+        System.out.println(hash);
+        hash.remove(3);
+        System.out.println(hash);
     }
 }
