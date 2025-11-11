e6e6dfd24e037274e3d9fb95cec094f0eef96caf
Sakthi <sakthikrishkmv@gmail.com>
2025-11-11 15:02:48 +0530

    HashMap

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      HashMap<Integer,String> Hm = new HashMap<>();
-      Hm.put(1,"Alvaro");
-      Hm.put(2,"Rodrygo");
-      System.out.println(Hm);
-      Hm.remove(1,"Alvaro");
-      System.out.println(Hm);
+     Queue<String> Qs = new LinkedList<>();
+     Qs.add("Mortal");
+     Qs.add("Sesko");
+     Qs.add("Mbeumo");
+     System.out.println(Qs);
+     Qs.remove("Sesko");
+     System.out.println(Qs);
+
 
 
 
